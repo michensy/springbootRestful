f8a58238c81c8315f4523485464d8fff901bf02b
夏紫东 <zidong.me@gmail.com>
2018-08-14 13:13:37 +0800

    框架初始化

---
+++ src/Test/java/com/sf/restful/RestfulTest.java
@@ -0,0 +1,16 @@
+package com.sf.restful;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestfulTest {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
---
+++ src/main/java/com/sf/config/CoreConfig.java
@@ -0,0 +1,13 @@
+package com.sf.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Zidong on 2018/6/7 13:58
+ */
+@Configuration
+@Component
+public class CoreConfig {
+
+}
